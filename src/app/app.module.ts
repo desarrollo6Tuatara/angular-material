3fc22f6fa092b0655e4995f621862717704b5c6c
Juan Pablo Betancur M <jpbetancur1999@gmail.com>
2022-10-20 17:04:08 -0500

    Conociendo las tipografías con angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MaterialModule } from './material/material.module'
+import { MaterialModule } from './material/material.module';
+import { TypographyComponent } from './components/typography/typography.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TypographyComponent
   ],
   imports: [
     BrowserModule,
